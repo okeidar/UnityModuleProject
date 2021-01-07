027b583d17b2c48194b3a6e0328c132c0e2403be
okeidar <33117174+okeidar@users.noreply.github.com>
2021-01-07 18:05:19 +0200

    pickup

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     [SerializeField] Weapon grantedItem; //TODO: should be more general
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("hi");
         var player = collision.gameObject.GetComponent<PlayerController>();
         if (player)
         {
