07ef438c224c454fcd682b04c24415789c4334a0
Adishsh <Adishsh@users.noreply.github.com>
2021-01-19 16:57:50 +0200

    added wall with collidersr

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private Collider2D m_Collider;
+    private Scannable m_l;
+    private void Awake() 
+    {
+        m_Collider = gameObject.GetComponent<Collider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        var l = other.gameObject.GetComponent<Scannable>();
+        if(l)
+        {
+            m_l = l;
+            m_Collider.isTrigger = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        var l = other.gameObject.GetComponent<Scannable>();
+        if(l && m_l == l)
+        {
+            m_Collider.isTrigger = true;
+        }
+    }
+}
